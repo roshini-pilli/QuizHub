abbbf9f514a67630b1ed97db081477b240befb7a
Roshini <roshhini18@gmail.com>
2026-09-19 12:43:39 +0530

    Fix production email SMTP connection

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,6 +3,8 @@ import nodemailer from "nodemailer";
 const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
